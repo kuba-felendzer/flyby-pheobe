a4e32a4b8d6ed420b3a84923abdabc12f258f8a2
Kuba Felendzer <kuba.felendzer@icloud.com>
2022-03-02 22:10:12 -0600

    Texturing test

--- src/main.c
+++ src/main.c
@@ -1,5 +1,27 @@
 #include <raylib.h>
 
+typedef struct {
+	char * PATH;
+	int ID;
+	Texture2D TEXTURE;
+} Tile;
+
+int tile_counter = 0x64; // used to assigned tiles their ids, incremented whenever load_tile() is called
+
+// loads a tile from a png image
+Tile load_tile(char * path) {
+	Tile output;
+
+	output.PATH = path;
+	
+	output.ID   = tile_counter;
+	tile_counter++;
+
+	output.TEXTURE = LoadTexture(path);
+
+	return output;
+}
+
 int main(void)
 {
     const int WIDTH = 800;
@@ -10,6 +32,10 @@ int main(void)
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
 
+	// Texture Loading
+	const Tile FLOOR_1 = load_tile("./res/floor-tile-1.png");
+
+
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
@@ -18,9 +44,9 @@ int main(void)
         // Draw
         BeginDrawing();
 
-            ClearBackground(BLUE);
-
-            DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+            ClearBackground(RAYWHITE);
+			
+			DrawTexture(FLOOR_1.TEXTURE, WIDTH / 2 - 4, HEIGHT / 2 - 4, WHITE);
 
         EndDrawing();
     }
